791a9f71aab3e3283f9e7a6a170404ebb6d91fb8
Pierangelo Guerrero <pierangelo.guerrero@gmail.com>
2022-12-13 17:19:33 +0100

    done

---
+++ src/components/Header.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>Hello {{ name }}</h1>
+
+  <button @click="helloWorld()">Button</button>
+</template>
+
+<script setup>
+import { ref } from "vue";
+let name = ref("pier");
+function helloWorld() {
+  console.log("Hello world");
+}
+</script>
+<style></style>
